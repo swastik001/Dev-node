241711237e81fa968b1b4a766d7a385a84905407
Swastik <swastik.ojha9@gmail.com>
2026-08-16 02:54:47 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,11 +2,28 @@ const exxpress = require("express");
 
 const app = exxpress();
 
-// request handler
+// app.use("/", (req, res) => {
+//   res.send("----");
+// });
+
+// This will only handle get call
+app.get("/user", (req, res) => {
+  res.send("firstName:Swastik");
+});
+
+app.post("/user", (req, res) => {
+  res.send("post request-data saved successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("delete request-data deleted successfully");
+});
+
+//use- this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send("hello from server");
 });
 
 app.listen(3000, () => {
-  console.log("sir-war");
+  console.log("server is up and running on port 3000");
 });
